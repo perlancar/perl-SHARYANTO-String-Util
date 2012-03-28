4abaaae2464fa2d8de2b8f0eb6b25efad41973b0
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-03-28 08:49:39 +0700

    Add ellipsis()

--- lib/SHARYANTO/String/Util.pm
+++ lib/SHARYANTO/String/Util.pm
@@ -7,7 +7,7 @@ use warnings;
 # VERSION
 
 our @ISA = qw(Exporter);
-our @EXPORT_OK = qw(trim_blank_lines);
+our @EXPORT_OK = qw(trim_blank_lines ellipsis);
 
 sub trim_blank_lines {
     local $_ = shift;
@@ -15,6 +15,18 @@ sub trim_blank_lines {
     s/\A(?:\n\s*)+//;
     s/(?:\n\s*){2,}\z/\n/;
     $_;
+}
+
+sub ellipsis {
+    my ($str, $maxlen, $ellipsis) = @_;
+    $maxlen   //= 80;
+    $ellipsis //= "...";
+
+    if (length($str) <= $maxlen) {
+        return $str;
+    } else {
+        return substr($str, 0, $maxlen-length($ellipsis)) . $ellipsis;
+    }
 }
 
 1;
@@ -26,6 +38,13 @@ sub trim_blank_lines {
 
 Trim blank lines at the beginning and the end. Won't trim blank lines in the
 middle. Blank lines include lines with only whitespaces in them.
+
+
+=head2 ellipsis($str[, $maxlen, $ellipsis]) => STR
+
+Return $str unmodified if $str's length is less than $maxlen (default 80).
+Otherwise cut $str to ($maxlen - length($ellipsis)) and append $ellipsis
+(default '...') at the end.
 
 
 =head1 SEE ALSO
--- t/string_util.t
+++ t/string_util.t
@@ -6,10 +6,17 @@ use warnings;
 
 use Test::More 0.96;
 
-use SHARYANTO::String::Util qw(trim_blank_lines);
+use SHARYANTO::String::Util qw(trim_blank_lines ellipsis);
 
 ok( !defined(trim_blank_lines(undef)), "trim_blank_lines undef" );
 is( trim_blank_lines("\n1\n\n2\n\n \n"), "1\n\n2\n", "trim_blank_lines 1" );
+
+is(ellipsis("", 10), "", "ellipsis 1");
+is(ellipsis("12345678", 10), "12345678", "ellipsis 1");
+is(ellipsis("1234567890", 10), "1234567890", "ellipsis 2");
+is(ellipsis("12345678901", 10), "1234567...", "ellipsis 3");
+is(ellipsis("123456789012345", 10), "1234567...", "ellipsis 4");
+is(ellipsis("12345678901", 10, "xxx"), "1234567xxx", "ellipsis 5");
 
 DONE_TESTING:
 done_testing();
