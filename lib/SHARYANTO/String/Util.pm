00af440d05a59197facc9933b95dee424718453a
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-07-11 19:47:33 +0700

    Copy-paste SHARYANTO::Utils' FAQ entry

--- lib/SHARYANTO/String/Util.pm
+++ lib/SHARYANTO/String/Util.pm
@@ -60,6 +60,26 @@ Indent every line in $str with $indent. Example:
  indent('  ', "one\ntwo\nthree") # "  one\n  two\n  three"
 
 
+=head1 FAQ
+
+=head2 What is this?
+
+This distribution is part of L<SHARYANTO::Utils>, a heterogenous collection of
+modules that will eventually have their own proper distributions, but do not yet
+because they are not ready for some reason or another. For example: alpha
+quality code, code not yet properly refactored, there are still no tests and/or
+documentation, I haven't decided on a proper name yet, etc.
+
+I put it on CPAN because some of my other modules (and scripts) depend on it.
+And I always like to put as much of my code in functions and modules (as opposed
+to scripts) as possible, for better reusability.
+
+You are free to use this, but beware that things might get moved around without
+prior warning.
+
+I assure you that this is not a vanity distribution :-)
+
+
 =head1 SEE ALSO
 
 L<SHARYANTO::Utils>
