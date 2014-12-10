e7f80401816187e18dbe00e962ed84573de58779
perlancar (@pc-office) <perlancar@gmail.com>
2014-12-10 18:04:08 +0700

    Spin off String-Trim-More, String-Pad, String-PerlQuote

--- lib/SHARYANTO/String/Util.pm
+++ lib/SHARYANTO/String/Util.pm
@@ -9,69 +9,12 @@ use warnings;
 use Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT_OK = qw(
-                       ltrim
-                       rtrim
-                       trim
-                       ltrim_lines
-                       rtrim_lines
-                       trim_lines
-                       trim_blank_lines
                        ellipsis
                        indent
                        linenum
-                       pad
-                       qqquote
-                       single_quote
-                       double_quote
                        common_prefix
                        common_suffix
                );
-
-sub ltrim {
-    my $str = shift;
-    $str =~ s/\A\s+//s;
-    $str;
-}
-
-sub rtrim {
-    my $str = shift;
-    $str =~ s/\s+\z//s;
-    $str;
-}
-
-sub trim {
-    my $str = shift;
-    $str =~ s/\A\s+//s;
-    $str =~ s/\s+\z//s;
-    $str;
-}
-
-sub ltrim_lines {
-    my $str = shift;
-    $str =~ s/^[ \t]+//mg; # XXX other unicode non-newline spaces
-    $str;
-}
-
-sub rtrim_lines {
-    my $str = shift;
-    $str =~ s/[ \t]+$//mg;
-    $str;
-}
-
-sub trim_lines {
-    my $str = shift;
-    $str =~ s/^[ \t]+//mg;
-    $str =~ s/[ \t]+$//mg;
-    $str;
-}
-
-sub trim_blank_lines {
-    local $_ = shift;
-    return $_ unless defined;
-    s/\A(?:\n\s*)+//;
-    s/(?:\n\s*){2,}\z/\n/;
-    $_;
-}
 
 sub ellipsis {
     my ($str, $maxlen, $ellipsis) = @_;
@@ -118,75 +61,6 @@ sub linenum {
     $str;
 }
 
-sub pad {
-    my ($text, $width, $which, $padchar, $is_trunc) = @_;
-    if ($which) {
-        $which = substr($which, 0, 1);
-    } else {
-        $which = "r";
-    }
-    $padchar //= " ";
-
-    my $w = length($text);
-    if ($is_trunc && $w > $width) {
-        $text = substr($text, 0, $width, 1);
-    } else {
-        if ($which eq 'l') {
-            $text = ($padchar x ($width-$w)) . $text;
-        } elsif ($which eq 'c') {
-            my $n = int(($width-$w)/2);
-            $text = ($padchar x $n) . $text . ($padchar x ($width-$w-$n));
-        } else {
-            $text .= ($padchar x ($width-$w));
-        }
-    }
-    $text;
-}
-
-# BEGIN COPY PASTE FROM Data::Dump
-my %esc = (
-    "\a" => "\\a",
-    "\b" => "\\b",
-    "\t" => "\\t",
-    "\n" => "\\n",
-    "\f" => "\\f",
-    "\r" => "\\r",
-    "\e" => "\\e",
-);
-
-# put a string value in double quotes
-sub double_quote {
-  local($_) = $_[0];
-  # If there are many '"' we might want to use qq() instead
-  s/([\\\"\@\$])/\\$1/g;
-  return qq("$_") unless /[^\040-\176]/;  # fast exit
-
-  s/([\a\b\t\n\f\r\e])/$esc{$1}/g;
-
-  # no need for 3 digits in escape for these
-  s/([\0-\037])(?!\d)/sprintf('\\%o',ord($1))/eg;
-
-  s/([\0-\037\177-\377])/sprintf('\\x%02X',ord($1))/eg;
-  s/([^\040-\176])/sprintf('\\x{%X}',ord($1))/eg;
-
-  return qq("$_");
-}
-# END COPY PASTE FROM Data::Dump
-
-# old name, deprecated, will be removed in the future
-sub qqquote { goto &double_quote; }
-
-# will write this in the future, will produce "qq(...)" and "q(...)" literal
-# representation
-#sub qq_quote {}
-#sub q_quote {}
-
-sub single_quote {
-  local($_) = $_[0];
-  s/([\\'])/\\$1/g;
-  return qq('$_');
-}
-
 sub common_prefix {
     return undef unless @_;
     my $i;
@@ -227,45 +101,6 @@ sub common_suffix {
 =for Pod::Coverage ^(qqquote)$
 
 =head1 FUNCTIONS
-
-=head2 ltrim($str) => STR
-
-Trim whitespaces (including newlines) at the beginning of string. Equivalent to:
-
- $str =~ s/\A\s+//s;
-
-=head2 ltrim_lines($str) => STR
-
-Trim whitespaces (not including newlines) at the beginning of each line of
-string. Equivalent to:
-
- $str =~ s/^\s+//mg;
-
-=head2 rtrim($str) => STR
-
-Trim whitespaces (including newlines) at the end of string. Equivalent to:
-
- $str =~ s/[ \t]+\z//s;
-
-=head2 rtrim_lines($str) => STR
-
-Trim whitespaces (not including newlines) at the end of each line of
-string. Equivalent to:
-
- $str =~ s/[ \t]+$//mg;
-
-=head2 trim($str) => STR
-
-ltrim + rtrim.
-
-=head2 trim_lines($str) => STR
-
-ltrim_lines + rtrim_lines.
-
-=head2 trim_blank_lines($str) => STR
-
-Trim blank lines at the beginning and the end. Won't trim blank lines in the
-middle. Blank lines include lines with only whitespaces in them.
 
 =head2 ellipsis($str[, $maxlen, $ellipsis]) => STR
 
@@ -324,43 +159,6 @@ If set to false, keep printing line number even if line is empty:
      4|line4
 
 =back
-
-=head2 pad($text, $width[, $which[, $padchar[, $truncate]]]) => STR
-
-Return C<$text> padded with C<$padchar> to C<$width> columns. C<$which> is
-either "r" or "right" for padding on the right (the default if not specified),
-"l" or "left" for padding on the right, or "c" or "center" or "centre" for
-left+right padding to center the text.
-
-C<$padchar> is whitespace if not specified. It should be string having the width
-of 1 column.
-
-=head2 double_quote($str) => STR
-
-Quote or encode C<$str> to the Perl double quote (C<">) literal representation
-of the string. Example:
-
- say double_quote("a");        # => "a"     (with the quotes)
- say double_quote("a\n");      # => "a\n"
- say double_quote('"');        # => "\""
- say double_quote('$foo');     # => "\$foo"
-
-This code is taken from C<quote()> in L<Data::Dump>. Maybe I didn't look more
-closely, but I couldn't a module that provides a function to do something like
-this. L<String::Escape>, for example, provides C<qqbackslash> but it does not
-escape C<$>.
-
-=head2 single_quote($str) => STR
-
-Like C<double_quote> but will produce a Perl single quote literal representation
-instead of the double quote ones. In single quotes, only literal backslash C<\>
-and single quote character C<'> are escaped, the rest are displayed as-is, so
-the result might span multiple lines or contain other non-printable characters.
-
- say single_quote("Mom's");    # => 'Mom\'s' (with the quotes)
- say single_quote("a\\");      # => 'a\\"
- say single_quote('"');        # => '"'
- say single_quote("\$foo");    # => '$foo'
 
 =head2 common_prefix(@LIST) => STR
 
--- t/string_util.t
+++ t/string_util.t
@@ -7,79 +7,12 @@ use warnings;
 use Test::More 0.98;
 
 use SHARYANTO::String::Util qw(
-                                  ltrim
-                                  rtrim
-                                  trim
-                                  ltrim_lines
-                                  rtrim_lines
-                                  trim_lines
-                                  trim_blank_lines
                                   ellipsis
                                   indent
                                   linenum
-                                  pad
-                                  double_quote
-                                  single_quote
                                   common_prefix
                                   common_suffix
                           );
-
-subtest ltrim => sub {
-    is(ltrim(" \ta")             , "a");
-    is(ltrim("\n \ta")           , "a");
-    is(ltrim("a \t")             , "a \t");
-    is(ltrim("a\n \t")           , "a\n \t");
-    is(ltrim(" \ta\n b\n")       , "a\n b\n");
-    is(ltrim("a\n \tb \n\n")     , "a\n \tb \n\n");
-};
-
-subtest rtrim => sub {
-    is(rtrim(" \ta")             , " \ta");
-    is(rtrim("\n \ta")           , "\n \ta");
-    is(rtrim("a \t")             , "a");
-    is(rtrim("a\n \t")           , "a");
-    is(rtrim(" \ta\n b\n")       , " \ta\n b");
-    is(rtrim("a\n \tb \n\n")     , "a\n \tb");
-};
-
-subtest trim => sub {
-    is(trim(" \ta")             , "a");
-    is(trim("\n \ta")           , "a");
-    is(trim("a \t")             , "a");
-    is(trim("a\n \t")           , "a");
-    is(trim(" \ta\n b\n")       , "a\n b");
-    is(trim("a\n \tb \n\n")     , "a\n \tb");
-};
-
-subtest ltrim_lines => sub {
-    is(ltrim_lines(" \ta")             , "a");
-    is(ltrim_lines("\n \ta")           , "\na");
-    is(ltrim_lines("a \t")             , "a \t");
-    is(ltrim_lines("a\n \t")           , "a\n");
-    is(ltrim_lines(" \ta\n b\n")       , "a\nb\n");
-    is(ltrim_lines("a\n \tb \n\n")     , "a\nb \n\n");
-};
-
-subtest rtrim_lines => sub {
-    is(rtrim_lines(" \ta")             , " \ta");
-    is(rtrim_lines("\n \ta")           , "\n \ta");
-    is(rtrim_lines("a \t")             , "a");
-    is(rtrim_lines("a\n \t")           , "a\n");
-    is(rtrim_lines(" \ta\n b\n")       , " \ta\n b\n");
-    is(rtrim_lines("a\n \tb \n\n")     , "a\n \tb\n\n");
-};
-
-subtest trim_lines => sub {
-    is(trim_lines(" \ta")             , "a");
-    is(trim_lines("\n \ta")           , "\na");
-    is(trim_lines("a \t")             , "a");
-    is(trim_lines("a\n \t")           , "a\n");
-    is(trim_lines(" \ta\n b\n")       , "a\nb\n");
-    is(trim_lines("a\n \tb \n\n")     , "a\nb\n\n");
-};
-
-ok( !defined(trim_blank_lines(undef)), "trim_blank_lines undef" );
-is( trim_blank_lines("\n1\n\n2\n\n \n"), "1\n\n2\n", "trim_blank_lines 1" );
 
 is(ellipsis("", 10), "", "ellipsis 1");
 is(ellipsis("12345678", 10), "12345678", "ellipsis 1");
@@ -100,29 +33,6 @@ is(linenum($str, {zeropad=>1}),
    "0001|a\n0002|b\n    |\n0004| d\n0005|0", "linenum opt:zeropad");
 is(linenum($str, {skip_empty=>0}),
    "   1|a\n   2|b\n   3|\n   4| d\n   5|0", "linenum opt:skip_empty");
-
-subtest "pad" => sub {
-    is(pad("1234", 4), "1234");
-    is(pad("1234", 6), "1234  ", "right");
-    is(pad("1234", 6, "l"), "  1234", "left");
-    is(pad("1234", 6, "c"), " 1234 ", "center");
-    is(pad("1234", 6, "c", "x"), "x1234x", "padchar");
-    is(pad("1234", 1), "1234", "trunc=0");
-    is(pad("1234", 1, undef, undef, 1), "1", "trunc=1");
-};
-
-subtest "double_quote" => sub {
-    is(double_quote("a"),    '"a"');
-    is(double_quote("a\n"),  '"a\\n"');
-    is(double_quote('"'),    '"\\""');
-    is(double_quote('$foo'), '"\\$foo"');
-};
-
-subtest "single_quote" => sub {
-    is(single_quote("a\"'\$\\"), qq('a"\\'\$\\\\'));
-    is(single_quote("a\nb"), q('a
-b'));
-};
 
 subtest "common_prefix" => sub {
     is(common_prefix(""), "");
